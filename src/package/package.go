dd315be7bd2f11bd8d53b16f5b62f7b9cac85a00
William Bell <epicugric@gmail.com>
2023-03-21 23:20:29 +0000

    add first commands

---
+++ src/package/package.go
@@ -0,0 +1,31 @@
+package pack
+
+import (
+	"github.com/Open-Argon/Isotope/src/args"
+	"github.com/Open-Argon/Isotope/src/help"
+	"github.com/Open-Argon/Isotope/src/package/zip"
+)
+
+var usage = `isotope pack [options]`
+var o = help.Options{
+	{"pack an module into a package", "zip [path]"},
+	{"push a module to a remote", "push [package name]"},
+	{"list all zipped packages", "list"},
+	{"show help", "--help, -h"},
+}
+
+func Pack() {
+	args := args.Args[1:]
+	if len(args) == 0 {
+		help.Help(usage, o)
+		return
+	}
+	switch args[0] {
+	case "--help", "-h":
+		help.Help(usage, o)
+	case "zip":
+		zip.Zip()
+	case "push":
+		push()
+	}
+}
